c859f44f9cdaa0d2baa5fc6c54c23dac2c4f149f
Michael Gates <michaelgatesdev@gmail.com>
2015-05-13 21:58:03 -0400

    Created new animation system

--- Assets/Scripts/Player/TestPlayerAnimate.cs
+++ Assets/Scripts/Player/TestPlayerAnimate.cs
@@ -3,64 +3,61 @@ using System.Collections;
 
 public class TestPlayerAnimate : MonoBehaviour
 {
-    private Animator anim;
-    private bool jumping;
-    private bool sliding;
+	private Animator anim;
+	private bool jumping;
+	private bool sliding;
 
-    // Use this for initialization
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
     
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Jump();
-        } else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            Slide();
-        } else
-        {
-            //Run();
-        }
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			Jump ();
+		} else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			Slide ();
+		} else {
+			//Run();
+		}
+	}
 
-    private void Jump()
-    {
-        if (jumping)
-            return;
+	private void Jump ()
+	{
+		if (jumping)
+			return;
 
-        jumping = true;
+		jumping = true;
 
-        anim.SetInteger("state", 1);
+		anim.SetBool ("jumping", true);
 
-        Debug.Log("Jump");
-    }
+		Debug.Log ("Jump");
+	}
     
-    private void Slide()
-    {
-        if (sliding)
-            return;
+	private void Slide ()
+	{
+		if (sliding)
+			return;
 
-        sliding = true;
+		sliding = true;
 
-        anim.SetInteger("state", 2);
+		anim.SetBool ("sliding", true);
+       
+		Debug.Log ("Slide");
+	}
+    
+	private void Run ()
+	{
+		if (anim.GetInteger ("state") == 0)
+			return;
+
+		anim.SetInteger ("state", 0);
         
-        Debug.Log("Slide");
-    }
-    
-    private void Run()
-    {
-        if (anim.GetInteger("state") == 0)
-            return;
-
-        anim.SetInteger("state", 0);
-        
-        Debug.Log("Run");
-    }
+		Debug.Log ("Run");
+	}
 
 }
 
