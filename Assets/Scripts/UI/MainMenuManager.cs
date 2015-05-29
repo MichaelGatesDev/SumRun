d7e5abdac38c707b7e92b6e525c29c36e295f715
Michael Gates <michaelgatesdev@gmail.com>
2015-05-29 11:04:56 -0400

    Almost finished main menu, polished some ui animation, functional menu

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour
+{
+	// ========================================================================================\\
+
+	public GameObject mainGroup;
+	public GameObject optionsGroup;
+
+	// ========================================================================================\\
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		mainGroup = GameObject.Find("MainGroup");
+		optionsGroup = GameObject.Find ("OptionsGroup");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	// ========================================================================================\\
+
+
+	public void PressPlay ()
+	{
+		mainGroup.GetComponent<Animator>().SetBool("playing", true);
+		StartCoroutine("StartGame", 0.4f);
+	}
+
+	public void PressOptions ()
+	{
+		optionsGroup.GetComponent<Animator>().SetBool("optioning", true);
+
+	}
+
+	public void PressBackFromOptions()
+	{
+		optionsGroup.GetComponent<Animator>().SetBool("optioning", false);
+	}
+
+	public void PressQuit ()
+	{
+		mainGroup.GetComponent<Animator>().SetBool("quitting", true);
+		StartCoroutine("QuitGame", 0.4f);
+	}
+
+	public void PressDebug ()
+	{
+	}
+
+	// ========================================================================================\\
+	
+	private void StartGame()
+	{
+		Application.LoadLevel("Level");
+	}
+	
+	private void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+	
+	// ========================================================================================\\
+}
