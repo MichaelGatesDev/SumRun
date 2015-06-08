5aac17d425404a097efd73d4472c72d853dbde72
Michael Gates <michaelgatesdev@gmail.com>
2015-06-08 10:18:34 -0400

    minor ui changes

---
+++ Assets/Scripts/Mobile/ForceOrientation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceOrientation : MonoBehaviour
+{
+	// ========================================================================================\\
+
+	public enum OrientationMode
+	{
+		LANDSCAPE,
+		PORTRAIT,
+	}
+	
+	// ========================================================================================\\
+
+	public OrientationMode mode; // the orientation 
+	
+	// ========================================================================================\\
+
+	// Use this for initialization
+	void Start ()
+	{
+		// if landscape mode
+		if (mode == OrientationMode.LANDSCAPE) {
+			// set landscape mode
+			Screen.orientation = ScreenOrientation.LandscapeLeft;
+		} else {
+			// set portrait mode
+			Screen.orientation = ScreenOrientation.Portrait;
+		}
+	}
+	
+	// ========================================================================================\\
+}
---
+++ Assets/Scripts/Util/Indestructable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Indestructable : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		// makes object persistent
+		DontDestroyOnLoad(gameObject);
+	}
+
+}
